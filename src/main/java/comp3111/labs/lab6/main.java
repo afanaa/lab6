6db37d96847d08d24d746295e31d8cbeefddc9bb
Fan Arthur <afanaa@connect.ust.hk>
2021-11-03 11:02:01 +0800

    lab6 done

--- src/main/java/comp3111/labs/lab6/main.java
+++ src/main/java/comp3111/labs/lab6/main.java
@@ -6,16 +6,18 @@ package comp3111.labs.lab6;
 public class main {
 	public static void main(String[] args) {
 		Animal animals[] = new Animal[10];
-		for (Animal a : animals)
-			a = new Animal();	
+		for (int i=0;i<10;i++)//  the old way of running create animals will lead to null pointer error
+			animals[i] = new Animal();	// as such, I changed it to run of the array directly instead of using the other way
 		for (int iii = 0; iii < 10; iii++) {
 			int ii = 0;
-			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
-				System.out.print(animals[iii].getWeight() + " ");
-				animals[iii].eat();
-				if (ii % 3 == 0)
-				animals[iii].poo();
-			}
+			
+				for (; ii < 100 && animals[iii].isAlive() ; ii++) {
+					System.out.print(animals[iii].getWeight() + " ");
+					animals[iii].eat();
+					if (ii % 3 == 0)
+						animals[iii].poo();
+				}
+			
 		}	
 	}
 }
